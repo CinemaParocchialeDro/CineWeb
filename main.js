a17dfb6c88dfa46013ebd4117a29c4c71a803d51
CinemaParocchialeDro <info.cinemadro@gmail.com>
2025-10-05 22:12:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ const films = [
         note: ""
     },
     {
-        title: "Dalai lama - La Saggezza della Felicità",
+        title: "Dalai Lama - La Saggezza della Felicità",
         genre: "Documentario",
         duration: "94 min",
         poster: "immagini/DALAI LAMA.jpg",
@@ -280,6 +280,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     
 });
+
 
 
 
